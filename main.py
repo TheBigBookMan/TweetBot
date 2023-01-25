226be7cc5f34c4344316730e23c625c7631b6c94
Ben Smerd <bsmerd@adam.com.au>
2023-01-25 17:18:06 +1030

    added in options for the webdriver

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
+service = Service('C:\\Users\\bsmer\\Downloads\\chromedriver.exe')
+
+def get_driver():
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+
+  driver = webdriver.Chrome(service=service, options=options)
+  driver.get('https://twitter.com/i/flow/login')
+  return driver
